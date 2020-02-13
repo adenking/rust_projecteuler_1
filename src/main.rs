33fcbe805ddaf590ff864e882886f9634450299f
Aden King <adenking@live.com>
2020-02-13 15:01:07 +1000

    completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = Vec::new();
+    for num in 1..1000 {
+        if num % 3 == 0 {
+            numbers.push(num)
+        } else if num % 5 == 0 {
+            numbers.push(num)
+        }
+    }
+    let total: u32 = numbers.iter().sum();
+    println!("Sum of numbers is {}", total);
 }
